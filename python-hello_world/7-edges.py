99aecc89c4bb62b59f86ed18d37857d82479b003
sira-djam <b.soileux@gorange.fr>
2025-01-15 10:13:09 +0100

    imprimer des parties differentes d'un mot 7-edges

---
+++ python-hello_world/7-edges.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+word = "Holberton"
+# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+print(f"word first 3: {word[0:3]}")
+print(f"word last 2: {word[7:9]}")
+print(f"middle word: {word[1:8]}")
