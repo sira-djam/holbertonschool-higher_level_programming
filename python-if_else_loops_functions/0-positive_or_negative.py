ca2353d08a4c33338c2878b2cef58e625f44b81e
sira-djam <b.soileux@gorange.fr>
2025-01-16 14:15:24 +0100

    modif elif condition

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10, 10)
-if number < 0:
-    print(number, "is negative")
+
+if number > 0:
+    print(f"{number} is positive")
 elif number == 0:
-    print(number, "is zéro")
-elif number > 0:
-    print(number, "is positive")
+    print(f"{number} is zero")
+else:
+    print(f"{number} is negative")
