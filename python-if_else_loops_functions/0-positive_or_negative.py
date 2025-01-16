9884bcf2041dcb80fef3ecbc2c949af290f9349e
sira-djam <b.soileux@gorange.fr>
2025-01-16 14:06:24 +0100

    rendre le fichier executable

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10, 10)
-for number in range(-10, 10):
-    if number < 0: 
-        print(number,"is negative")
-    elif number == 0:
-        print(number,"is zéro")
-    elif number > 0:
-        print(number,"is positive")
+if number < 0:
+ print(number,"is negative")
+elif number == 0:
+ print(number,"is zéro")
+elif number > 0:
+ print(number,"is positive")
