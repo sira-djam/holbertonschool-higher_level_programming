579830a1b2cbc3cd0530c4a528833d123a06b3bb
sira-djam <b.soileux@gorange.fr>
2025-01-22 11:30:52 +0100

    modif fichier tuple 7

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
+    # Assurer que tuple_a contient au moins 2 éléments
     if len(tuple_a) < 2:
-        if len(tuple_a) == 0:
-            tuple_a = 0, 0
-        else:
-            tuple_a = tuple_a[0], 0
-            if len(tuple_b) < 2:
-                if len(tuple_b) == 0:
-                    tuple_b = 0, 0
-                else:
-                    tuple_b = tuple_b[0], 0
-            return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+        tuple_a = tuple_a + (0,) * (2 - len(tuple_a))
+    else:
+        tuple_a = tuple_a[:2]
+
+    # Assurer que tuple_b contient au moins 2 éléments
+    if len(tuple_b) < 2:
+        tuple_b = tuple_b + (0,) * (2 - len(tuple_b))
+    else:
+        tuple_b = tuple_b[:2]
+
+    # Additionner les éléments correspondants
+    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
