14e2460ec9131b848ee4738392953d649484567b
sira-djam <b.soileux@gorange.fr>
2025-01-22 11:50:53 +0100

    modif fichier 10

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
- multiples = []
- for i in range(len(my_list)):
-        if my_list[i] % 2 == 0:
-            multiples.append(True)
+ # Initialise une liste vide pour stocker les résultats (True ou False)
+    result = []
+
+    # Parcourt chaque élément `x` de la liste `my_list`
+    for x in my_list:
+        # Vérifie si le nombre est divisible par 2
+        if x % 2 == 0:
+            # Si oui, ajoute True à la liste `result`
+            result.append(True)
         else:
-            multiples.append(False)
-            return (multiples)
+            # Sinon, ajoute False à la liste `result`
+            result.append(False)
+
+    # Retourne la liste contenant True ou False pour chaque élément
+    return(result)
