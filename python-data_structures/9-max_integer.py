174aa8a4e8bb8898f99f8db46b4d7a8149d3d4d1
sira-djam <b.soileux@gorange.fr>
2025-01-22 11:47:11 +0100

    modif fichier 9

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,9 +1,10 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 def max_integer(my_list=[]):
-    if not my_list:
+    if not my_list:  # Si la liste est vide
         return None
-    max_val = my_list[0]
-    for num in my_list:
-        if num > max_val:
-            max_val = num
-    return max_val
+
+    max_val = my_list[0]  # On initialise avec le premier élément
+    for num in my_list:  # On parcourt tous les éléments
+        if num > max_val:  # Si un élément est plus grand que max_val
+            max_val = num  # On met à jour max_val
+    return max_val  # On retourne la valeur maximale
