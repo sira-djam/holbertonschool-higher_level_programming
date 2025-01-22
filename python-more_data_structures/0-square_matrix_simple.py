3c36c036b0b2e07dcabfbb70239bac64e3073c4c
sira-djam <b.soileux@gorange.fr>
2025-01-22 15:15:41 +0100

    modif

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    squared_matrix = []
-    for row in matrix:
-        squared_row = []
-        for x in row:
-            squared_row.append(x * x)
-        squared_matrix.append(squared_row)
-    return squared_matrix
-
+    return [[element ** 2 for element in row] for row in matrix]
 
