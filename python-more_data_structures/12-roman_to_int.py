e9d6e868843eb64f0b35eb8058bf8b032bab4391
sira-djam <b.soileux@gorange.fr>
2025-01-23 15:59:52 +0100

    modif 12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string == "":
+    if roman_string is None:
         return 0
     roman_number = {
         "I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
