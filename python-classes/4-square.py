92da3d4916f7b8cf0a7c09181dea2593077ea528
sira-djam <b.soileux@gorange.fr>
2025-01-28 13:57:31 +0100

    modif fichier 4

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -11,6 +11,6 @@ class Square:
                 raise TypeError("size must be integer")
             elif value < 0:
                 raise ValueError("size must be >= 0")
-        self.__size = value
+        self.__size = size
         def area(self):
             return (self.__size * self.__size)
