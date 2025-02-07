919ba619ebf784086c2b3900da84bbf060a3a6e3
sira-djam <b.soileux@gorange.fr>
2025-02-07 15:27:54 +0100

    modification fichier 3 et 5

--- python-abc/task_03_countediterator.py
+++ python-abc/task_03_countediterator.py
@@ -1,36 +1,16 @@
-#!/usr/bin/python3
-# Define the CountedIterator class which extends the built-in iterator
+#!/usr/bin/env python3
 class CountedIterator:
     def __init__(self, iterable):
-        # Initialize with the given iterable and set the counter to 0
-        self.iterable = iterable
-        self.iterator = iter(iterable)  # Create an iterator from the iterable
-        self.count = 0  # Initialize the counter to 0
+        self.iterator = iter(iterable)
+        self.count = 0
 
-    # Override the __next__ method to track the number of items iterated over
+    def __iter__(self):
+        return self
+
     def __next__(self):
-        try:
-            # Fetch the next item from the iterator
-            item = next(self.iterator)
-            # Increment the count
-            self.count += 1
-            # Return the item
-            return item
-        except StopIteration:
-            # If iteration is finished, raise StopIteration to indicate the end
-            raise StopIteration
+        item = next(self.iterator)
+        self.count += 1
+        return item
 
-    # Optional: method to get the count of items iterated so far
     def get_count(self):
         return self.count
-
-
-# Test the CountedIterator class
-items = [10, 20, 30, 40, 50]
-
-# Create an instance of CountedIterator
-counted_iter = CountedIterator(items)
-
-# Iterate over the items and print them along with the count
-for item in counted_iter:
-    print(f"Item: {item}, Count: {counted_iter.get_count()}")
--- python-abc/task_5_dragon.py
+++ python-abc/task_5_dragon.py
@@ -1,23 +1,12 @@
-#!/usr/bin/python3
-# Mixin for swimming abilities
+#!/usr/bin/env python3
 class SwimMixin:
     def swim(self):
         print("The creature swims!")
 
-# Mixin for flying abilities
 class FlyMixin:
     def fly(self):
         print("The creature flies!")
 
-# Dragon class that inherits from both SwimMixin and FlyMixin
 class Dragon(SwimMixin, FlyMixin):
     def roar(self):
         print("The dragon roars!")
-
-# Instantiate a Dragon object named draco
-draco = Dragon()
-
-# Demonstrate draco's abilities
-draco.swim()  # Output: The creature swims!
-draco.fly()   # Output: The creature flies!
-draco.roar()  # Output: The dragon roars!
