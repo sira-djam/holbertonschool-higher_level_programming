412698188f559e81edcbfd7be282924b5f578415
sira-djam <b.soileux@gorange.fr>
2025-02-10 15:38:58 +0100

    insertion commentaires

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def read_file(filename=""):
+    # Open the file with the given filename using 'utf-8' encoding
     with open(filename, encoding="utf-8") as file:
+        # Read and print the content of the file without adding extra newlines
         print(file.read(), end="")
