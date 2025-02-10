46beb89985382c03526604d2fc8e3c64056a8841
sira-djam <b.soileux@gorange.fr>
2025-02-10 15:37:01 +0100

    insertion commentaire

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 def write_file(filename="", text=""):
-    with open(filename, mode='w', encoding='utf-8') as file:
+    # Ouvre le fichier en mode écriture ('w') avec l'encodage UTF-8.
+    """ Si le fichier n'existe pas, il sera créé. Si le fichier existe déjà,
+    son contenu sera écrasé."""
+    with open(filename, mode="w", encoding="utf-8") as file:
+        # Écrit le texte dans le fichier et retourne le nombre de caractères écrits
         return file.write(text)
