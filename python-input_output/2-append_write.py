8d3adc5655f7ec7b0e8b7918bdc190ca5583a38f
sira-djam <b.soileux@gorange.fr>
2025-02-11 10:54:24 +0100

    modification des commentaires fichier 2

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """use fonction who use append"""
 
+
 def write_file(filename="", text=""):
  """Opens the file in write mode ('w') with UTF-8 encoding."""
  with open(filename, mode="w", encoding="utf-8") as file:
