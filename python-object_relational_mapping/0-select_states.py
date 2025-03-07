281dd5d54bfd2c0f9c32f18a0dce7291a8d1d619
sira-djam <b.soileux@gorange.fr>
2025-03-07 10:41:57 +0100

    fichier 0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-""" This script return all states """
-if __name__ == '__main__':
-    import MySQLdb
-    from sys import argv
-    db = MySQLdb.connect(host="localhost",pyth
-                         port=3306,
-                         user=argv[1],
-                         passwd=argv[2],
-                         db=argv[3])
+"""Module listing all states from the database"""
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3]
+    )
     cur = db.cursor()
     cur.execute("""SELECT * FROM states ORDER BY id""")
     rows = cur.fetchall()
