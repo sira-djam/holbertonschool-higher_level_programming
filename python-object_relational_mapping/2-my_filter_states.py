7b5ef43d9d7512ce62a7159fbfccbb7ecf2153f8
sira-djam <b.soileux@gorange.fr>
2025-03-07 11:25:25 +0100

    fichier 2

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
-""" This script return filter states """
-if __name__ == '__main__':
-    import MySQLdb
-    from sys import argv
-    db = MySQLdb.connect(host="localhost",
-                         port=3306,
-                         user=argv[1],
-                         passwd=argv[2],
-                         db=argv[3])
+"""Module listing all Arizona name from the database"""
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect("localhost", argv[1], argv[2], argv[3])
     cur = db.cursor()
-    cur.execute("""SELECT * FROM states
-                WHERE name LIKE BINARY '{}'
-                ORDER BY id""".format(argv[4]))
+    cur.execute(
+        """SELECT * FROM states WHERE name = '{}' ORDER BY id ASC"""
+        .format(argv[4]))
     rows = cur.fetchall()
     for row in rows:
-        print(row)
+        if row[1] == argv[4]:
+            print(row)
     cur.close()
     db.close()
