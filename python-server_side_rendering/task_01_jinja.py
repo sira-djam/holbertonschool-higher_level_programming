728e92940123a3bed51e72c667689f97d2e7ae8d
sira-djam <b.soileux@gorange.fr>
2025-03-27 09:31:09 +0100

    task 01

--- python-server_side_rendering/task_01_jinja.py
+++ python-server_side_rendering/task_01_jinja.py
@@ -2,21 +2,20 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
+# Route pour la page d'accueil
 @app.route('/')
 def home():
     return render_template('index.html')
 
-
+# Route pour la page "About"
 @app.route('/about')
 def about():
     return render_template('about.html')
 
-
+# Route pour la page "Contact"
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
-
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
